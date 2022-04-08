cb0e72d76cbcb9d79f268f04e5183008d42b57f6
izemskov <pascal.ilya@gmail.com>
2022-04-08 23:36:38 +0300

    JavaEE RESTful application

---
+++ JavaEE/JavaEERestAPI/src/main/java/ru/develgame/javaeerestapi/JaxRsConfig.java
@@ -0,0 +1,11 @@
+package ru.develgame.javaeerestapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Ilya Zemskov
+ */
+@ApplicationPath("resources")
+public class JaxRsConfig extends Application {
+}
