53fb082fff1f1235bb1365dacd29bb154b8b5761
izemskov <pascal.ilya@gmail.com>
2022-04-11 16:54:53 +0300

    Get data from database

--- JavaEE/JavaEERestAPI/src/main/java/ru/develgame/javaeerestapi/service/SC2UnitService.java
+++ JavaEE/JavaEERestAPI/src/main/java/ru/develgame/javaeerestapi/service/SC2UnitService.java
@@ -2,8 +2,13 @@ package ru.develgame.javaeerestapi.service;
 
 import ru.develgame.javaeerestapi.entity.SC2Unit;
 
-import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.inject.Named;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +17,37 @@ import java.util.List;
  */
 @Named
 public class SC2UnitService {
-    private List<SC2Unit> sc2UnitList = new ArrayList<>();
-
-    @PostConstruct
-    public void init() {
-        SC2Unit zerling = new SC2Unit(1L, "Zerling", 1.0d, 1.0d);
-        sc2UnitList.add(zerling);
-    }
+    @Resource(lookup = "jdbc/javaeedb")
+    private DataSource dataSource;
 
     public List<SC2Unit> getSC2Units() {
-        return new ArrayList<>(sc2UnitList);
+        List<SC2Unit> sc2UnitList = new ArrayList<>();
+
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+
+            try (PreparedStatement statement = connection.prepareStatement("SELECT id, attack, defense, name FROM sc2units")) {
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    sc2UnitList.add(new SC2Unit(resultSet.getLong(1),
+                            resultSet.getString(4),
+                            resultSet.getDouble(2),
+                            resultSet.getDouble(3)));
+                }
+            }
+        } catch (SQLException e) {
+        }
+        finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                }
+                catch (SQLException e) {
+                }
+            }
+        }
+
+        return sc2UnitList;
     }
 }
