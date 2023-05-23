d1840a5c6790bc4121c95adbab99d3c01bfa5863
izemskov <pascal.ilya@gmail.com>
2023-05-23 14:41:30 +0300

    JavaEE JAAS

---
+++ JavaEE/JavaEEAuth/src/main/java/ru/develgame/javaeeauth/NoCacheFilter.java
@@ -0,0 +1,25 @@
+package ru.develgame.javaeeauth;
+
+import javax.faces.application.ResourceHandler;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class NoCacheFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        if (!req.getRequestURI().startsWith(req.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER)) {
+            res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+            res.setHeader("Pragma", "no-cache");
+            res.setDateHeader("Expires", 0);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
