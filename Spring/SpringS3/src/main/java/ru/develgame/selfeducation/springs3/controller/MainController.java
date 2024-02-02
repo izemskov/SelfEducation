59c7abb9735ff03eaddf5f4da56378be75fd4d8a
izemskov <pascal.ilya@gmail.com>
2024-02-02 17:09:48 +0300

    Spring S3 exceptions

--- Spring/SpringS3/src/main/java/ru/develgame/selfeducation/springs3/controller/MainController.java
+++ Spring/SpringS3/src/main/java/ru/develgame/selfeducation/springs3/controller/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ru.develgame.selfeducation.springs3.dto.S3UploadedFileDtoResp;
+import ru.develgame.selfeducation.springs3.exception.S3CannotUploadFileException;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -27,14 +28,22 @@ public class MainController {
     }
 
     @PostMapping("upload")
-    public ResponseEntity<S3UploadedFileDtoResp> update(@RequestPart("file") MultipartFile file) throws IOException, InterruptedException {
+    public ResponseEntity<S3UploadedFileDtoResp> update(@RequestPart("file") MultipartFile file) {
         String key = UUID.randomUUID().toString();
 
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(file.getSize());
-        // TODO - IOException, InterruptedException
-        Upload upload = transferManager.upload(bucketName, key, file.getInputStream(), metadata);
-        upload.waitForUploadResult();
+        Upload upload = null;
+        try {
+            upload = transferManager.upload(bucketName, key, file.getInputStream(), metadata);
+            upload.waitForUploadResult();
+        } catch (IOException ex) {
+            throw new S3CannotUploadFileException("Cannot upload file");
+        }
+        catch (InterruptedException ex) {
+            upload.abort();
+            throw new S3CannotUploadFileException("Cannot upload file: interrupted");
+        }
 
         return ResponseEntity.ok(new S3UploadedFileDtoResp(key));
     }
---
+++ Spring/SpringS3/src/main/java/ru/develgame/selfeducation/springs3/exception/S3CannotUploadFileException.java
@@ -0,0 +1,7 @@
+package ru.develgame.selfeducation.springs3.exception;
+
+public class S3CannotUploadFileException extends RuntimeException {
+    public S3CannotUploadFileException(String message) {
+        super(message);
+    }
+}
