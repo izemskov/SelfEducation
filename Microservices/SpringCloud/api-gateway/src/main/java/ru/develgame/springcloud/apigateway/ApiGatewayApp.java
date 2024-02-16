c352107d3b1e72653bb693be79ec9a0fde50952f
izemskov <pascal.ilya@gmail.com>
2024-02-16 14:02:55 +0300

    Api gateway simple routing

---
+++ Microservices/SpringCloud/api-gateway/src/main/java/ru/develgame/springcloud/apigateway/ApiGatewayApp.java
@@ -0,0 +1,11 @@
+package ru.develgame.springcloud.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApp.class, args);
+    }
+}
