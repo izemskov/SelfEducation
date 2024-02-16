41d98cd451f601eee6c8b2c2a846d9f37eab62ea
izemskov <pascal.ilya@gmail.com>
2024-02-16 14:21:01 +0300

    Api gateway eureka routing

--- Microservices/SpringCloud/api-gateway/src/main/java/ru/develgame/springcloud/apigateway/ApiGatewayApp.java
+++ Microservices/SpringCloud/api-gateway/src/main/java/ru/develgame/springcloud/apigateway/ApiGatewayApp.java
@@ -2,7 +2,9 @@ package ru.develgame.springcloud.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApp {
     public static void main(String[] args) {
