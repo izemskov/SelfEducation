7bc0fe950cbe703a6d7fe222ce2e7d48fffa0137
izemskov <pascal.ilya@gmail.com>
2024-02-19 22:35:10 +0300

    CORS

---
+++ Spring/SpringSecurity/CORS/server-app/src/main/java/ru/develgame/springcors/server/configuration/CorsConfig.java
@@ -0,0 +1,34 @@
+package ru.develgame.springcors.server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("http://localhost:8082, https://www.google.com, https://google.com");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(List.of(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+        );
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
